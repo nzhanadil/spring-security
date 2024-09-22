b8322f9af86f5400ee0d789038d519cca424db23
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-09-22 17:30:05 -0500

    first commit

---
+++ src/main/java/com/vinci/security/config/JwtService.java
@@ -0,0 +1,33 @@
+package com.vinci.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "r2dckLM7VHEuhQm4EzyUNOy/Zqg3torApks9kXndVGVtbYCTSnkwNC49m5LlgHkr";
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ src/main/java/com/vinci/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.vinci.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
